0ae963c2ad9d62f8f9f22d950da7065faae57bb1
fatih kilic <fatihkilic956@hotmail.com>
2018-11-23 16:31:10 +0100

    login toegevoegd

---
+++ src/sample/LoginController.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField username;
+    private TextField password;
+
+    @FXML
+    public void login(ActionEvent event){
+
+
+    }
+
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,14 +18,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Kassa systeem");
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         try {
+            launch(args);
             Connection connection = SQLConnectionManager.getConnection();
 
             Statement statement = null;
