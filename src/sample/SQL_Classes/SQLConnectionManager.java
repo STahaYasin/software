f42c542998d54a53575f9d90a617731bb76fab44
TahaYasin <savrantaha@hotmail.com>
2018-12-01 02:14:50 +0100

    Added sql documents

--- src/sample/SQL_Classes/SQLConnectionManager.java
+++ src/sample/SQL_Classes/SQLConnectionManager.java
@@ -6,6 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 import java.util.TimeZone;
 
+/**
+ * Handles the sql connection to the database
+ */
 public class SQLConnectionManager {
 
     private static final String HOSTNAME = "localhost";
@@ -16,6 +19,11 @@ public class SQLConnectionManager {
 
     private static final String DATABASE = "software";
 
+    /**
+     *
+     * @return Resturns the created sql connection when a connection is successfully established
+     * @throws SQLException throws an exception if the server rejects the connection or when something goes wrong
+     */
     public static Connection getConnection() throws SQLException{
         Properties properties = new Properties();
         properties.put("user", USERNAME);
@@ -26,9 +34,22 @@ public class SQLConnectionManager {
 
         return DriverManager.getConnection(getUrl(HOSTNAME, PORT, DATABASE), properties);
     }
+
+    /**
+     * Private method creates a url in string format
+     * @param hostname
+     * @param port
+     * @param databasename
+     * @return
+     */
     private static String getUrl(String hostname, String port, String databasename){
         return "jdbc:mysql://" + hostname + ":" + port + "/" + databasename + "?useLegacyDatetimeCode=false&serverTimezone=America/New_York";
     }
+
+    /**
+     * Coses the connection to the server. every connection that's established with the server needs to be closed correctly
+     * @param connection the connection that needs to be closed.
+     */
     public static void closeConnection(Connection connection){
         try {
             connection.close();
